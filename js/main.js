7573b9f611083497c28c7e38a31608c34a36f247
buonoalex <loopatthecity@gmail.com>
2023-11-09 14:44:26 +0100

    add Hard-level

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@
 //Prendiamo i prompt 
 let name = prompt("Inserisci il tuo nome:");
 let surname = prompt("Inserisci il tuo cognome:");
-let numberdivisore = Number(prompt("Inserisci il divisore:"));
-let numberdividendo = Number(prompt("Inserisci il dividendo:"));
+let numberFirst = Number(prompt("Inserisci il primo numero:"));
+let numberSecond = Number(prompt("Inserisci il secondo numero:"));
 
 //Elaborazione
-let numberCalcolatedMedium = numberdivisore / numberdividendo
-let passwordBassLevel = name + "" + surname +21
+let numberCalcolatedMedium = Math.trunc(CalcoloMedio(numberFirst,numberSecond))
+let numberCalcolatedHard = Math.floor(Math.random() * 10000);
+let passwordBassLevel = name + "" + surname + 21
 let passwordMediumLevel = name + "" + surname + numberCalcolatedMedium
+let passwordHardLevel = name + "" + surname + numberCalcolatedHard
 
 //Visualizzare il risultato del prompt in HTML Bass Level
 document.getElementById("nameFirstBlock").innerHTML = name;
@@ -24,4 +26,31 @@ document.getElementById("passwordMediumLevel").innerHTML = passwordMediumLevel
 //Visualizzare il risultato del prompt in HTML Hard Level
 document.getElementById("nameThirdBlock").innerHTML = name;
 document.getElementById("surnameThirdBlock").innerHTML = surname;
-document.getElementById("passwordHardLevel").innerHTML = passwordMediumLevel
+document.getElementById("passwordHardLevel").innerHTML = passwordHardLevel
+
+//Funzione calcolo medio 
+function CalcoloMedio(x , y){
+
+    let scambio = 0;
+    let risultato = 0;
+
+    //ControlloZero(y);
+
+    if (x<y){
+        scambio = x;
+        x = y;
+        y = scambio;
+    }
+
+    risultato = x/y;
+    return risultato;
+}
+
+
+//Funzione controllo errore divisione 0
+/*function ControlloZero(z){
+    let avviso = "null";
+    if(z = 0){
+        avviso = "C'e stato un ERRORE non puoi divedere un numero per 0. (Riprova)"
+    }
+}*/
